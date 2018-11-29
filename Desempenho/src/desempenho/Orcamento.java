fd012ed1d7ad638c2e323ccfd183bf643f44b063
morettto <joao@uniplaclages.edu.br>
2018-11-29 03:56:26 -0200

    adicionando classe orcamento

---
+++ Desempenho/src/desempenho/Orcamento.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desempenho;
+
+/**
+ *
+ * @author joaoz
+ */
+public class Orcamento {
+private double valor;
+public Orcamento(double valor) {
+this.valor = valor;
+}
+public double getValor() {
+return valor;
+} 
+}
