d9e996cecbcfd2be7f68fee96f160d212a7dca45
morettto <joao@uniplaclages.edu.br>
2018-11-29 11:52:00 -0200

    Adicionando teste

---
+++ Desempenho/test/desempenho/ImpostoTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desempenho;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author joaoz
+ */
+public class ImpostoTest {
+    
+    public ImpostoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of calcula method, of class Imposto.
+     */
+    @Test
+    public void testCalcula() {
+        Orcamento orcamento = new Orcamento(100.0);
+Imposto imposto = new Imposto();
+// valor esperado, valor obtido
+assertEquals(10.0, imposto.calcula(orcamento), Double.MIN_VALUE);
+    }
+    
+}
